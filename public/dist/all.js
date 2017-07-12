403154fde49d5504f70ac20ddd1b8d99db6d9988
Taylor McKay <taylormckay68@gmail.com>
2017-07-12 16:06:56 -0600

    Got API pull working

--- public/dist/all.js
+++ public/dist/all.js
@@ -10,7 +10,29 @@ angular.module('group-project', ['ui.router']).config(function ($stateProvider, 
 });
 'use strict';
 
-angular.module('group-project').controller('mainCtrl', function ($scope, mainSvc) {});
+angular.module('group-project').service('flashCardSvc', function ($http) {
+    this.getJavascriptBasic = function () {
+        return $http({
+            url: '/api/getJSBasic',
+            method: 'GET'
+        });
+    };
+});
 'use strict';
 
 angular.module('group-project').service('mainSvc', function () {});
+'use strict';
+
+angular.module('group-project').controller('flashCardCtrl', function ($scope, flashCardSvc) {
+    $scope.recJsBasicData = function () {
+        flashCardSvc.getJavascriptBasic().then(function (response) {
+            console.log(response.data.terms);
+            $scope.data = response.data.terms;
+        });
+    };
+    $scope.recJsBasicData();
+    $scope.test = 'working';
+});
+'use strict';
+
+angular.module('group-project').controller('mainCtrl', function ($scope, mainSvc) {});
--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config');
-const app = express();
+const app = module.exports = express();
+const cors = require('cors');
+const axios = require('axios')
+const {getJSAll} = require('./Controllers/apiController.js');
+
+app.set('port', process.env.PORT || 3000)
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.static(__dirname + './../public'));
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.get('/api/getJSBasic', getJSAll)
+
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Listening on port ${app.get('port')}`);
 })
