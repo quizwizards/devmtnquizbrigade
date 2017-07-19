1e7cfbbe9798e4d7984dfdc0071274e999587b3f
Brock Pettyjohn <brockpettyjohn@gmail.com>
2017-07-19 09:40:39 -0600

    safety

--- public/app/directives/flashCardDirective.js
+++ public/app/directives/flashCardDirective.js
@@ -1,6 +1,11 @@
 angular.module('group-project')
 .directive('flashCards', function(){
     return {
-        templateUrl: './app/directives/flashCards-tmpl.html'
+        templateUrl: './app/directives/flashCards-tmpl.html',
+        controller: '.app/directives/flashCardCtrl',
+        scope: {
+            incrementButton: '&',
+            decrementButton: '&'
+        }
     }
 })
